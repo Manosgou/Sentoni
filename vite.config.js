2bf58e7d875ae1d49e30708abfd259a34290ff1c
Manos Gouvrikos <manolisgouvrikos@gmail.com>
2024-04-18 00:59:06 +0300

    Small fixes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { compression } from "vite-plugin-compression2";
 
-// https://vitejs.dev/config/
 export default defineConfig(async () => ({
   server: {
     port: 1420,
